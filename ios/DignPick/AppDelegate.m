46cb97e74906ecf7f5ae200a1e0939e205754990
Youssef Negm <usf.ngm@gmail.com>
2018-07-31 19:30:32 +0200

    hello

--- ios/DignPick/AppDelegate.m
+++ ios/DignPick/AppDelegate.m
@@ -10,10 +10,13 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import GoogleMaps; //add this line if you want to use Google Maps
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAo2IIk7dM6Xhk9zBkzSjUWfmE7WXzvYq0"];
   NSURL *jsCodeLocation;
 #ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
